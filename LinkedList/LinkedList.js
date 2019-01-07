829d87fe1f03231ca4530d43a97d96f18b3505df
Elzoka <mahmoud_elzoka@yahoo.com>
2019-01-07 02:16:14 +0200

    Add reverse function to the singly linked list

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -73,6 +73,26 @@ class LinkedList {
         this.length--;
         return this;
     }
+
+    reverse(){
+        if(!this.head.next){
+            return this.head;
+        }
+        let first = this.head;
+        this.tail = this.head;
+        let second = first.next;
+
+        while(second){
+            const temp = second.next;
+            second.next = first;
+            first = second;
+            second = temp; 
+        }
+
+        this.head.next = null;
+        this.head = first;
+        return this;
+    }
 }
 
 const myLinkedList = new LinkedList(10);
@@ -86,4 +106,6 @@ console.log(myLinkedList.printList());
 
 myLinkedList.remove(3);
 
+console.log(myLinkedList.printList());
+myLinkedList.reverse();
 console.log(myLinkedList.printList());
